ef1912b8cc7e1cb660c1fc4f14b1801d0ffc1ea2
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-05-15 17:43:08 +0200

    done

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,9 +1,9 @@
 import { useContext } from "react";
 import { BasketContext } from "../contexts/basket";
+import BasketItem from "./BasketItem";
 
 export default function Basket(props) {
-  const { basket, setBasket } = useContext(BasketContext);
-
+  const { basket } = useContext(BasketContext);
   const initialValue = 0;
   const sumWithInitial = basket.reduce(
     (previousValue, currentValue) =>
@@ -11,35 +11,15 @@ export default function Basket(props) {
     initialValue
   );
 
-  function remove(id) {
-    setBasket((old) => old.filter((item) => item.id !== id));
-  }
-
   return (
     <div>
       <ul>
         {basket.map((item) => {
-          return (
-            <li key={item.id}>
-              <button className="trash" onClick={() => remove(item.id)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-trash-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                </svg>
-              </button>
-              {item.amount} x {item.productdisplayname} ${item.price}
-            </li>
-          );
+          return <BasketItem key={item.id} {...item} />;
         })}
       </ul>
 
-      <p>Total: ${sumWithInitial}</p>
+      <p className="totalprice">Total: ${sumWithInitial}</p>
     </div>
   );
 }
